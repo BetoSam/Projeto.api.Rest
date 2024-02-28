ad30b1644382b9b4b7f37da52f3e12bc693cb8eb
BetoSam_git <jsbsam_lb@hotmail.com>
2024-02-28 20:37:27 -0300

    atualizamos o pom.xml e criamos uma classe de teste

---
+++ src/test/java/automacao/rest/MyTest.java
@@ -0,0 +1,19 @@
+package automacao.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class MyTest {
+Response response;
+	@Test
+	public void validarLista (){
+		response= RestAssured.get("https://reqres.in/api/users?page=2");
+		assertEquals(200, response.getStatusCode());
+	
+		
+	}
+}
